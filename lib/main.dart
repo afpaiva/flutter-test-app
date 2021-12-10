da4d3a7322ad0256da2c6d5c884e2db49f24b031
afpaiva <andrepaiva.arq@gmail.com>
2021-12-09 22:55:09 -0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -44,6 +44,60 @@ class MyApp extends StatelessWidget {
                   style: TextStyle(color: Colors.teal[100]),
                 ),
               ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 25.0,
+                  horizontal: 25.0,
+                ),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      '+55 35 99244 9829',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Graphik',
+                        fontSize: 20.0,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(
+                  horizontal: 25.0,
+                  vertical: 0.0,
+                ),
+                padding: EdgeInsets.all(10.0),
+                color: Colors.white,
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.mail,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'andrepaiva.arq@gmail.com',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Graphik',
+                        fontSize: 20.0,
+                      ),
+                    )
+                  ],
+                ),
+              )
             ],
           ),
         ),
